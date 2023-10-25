cb1658ddd59190a14229e83112831a8b1cad79a6
Rehanul Ahmed <ra3238159@gmail.com>
2023-10-25 16:38:13 +0600

    added new file and updated requirements.txt

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 from forms import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
