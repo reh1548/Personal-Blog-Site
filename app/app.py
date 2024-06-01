50d3afa4c30a77c21367337c39da146c06fec973
Rehanul Ahmed <79300086+reh1548@users.noreply.github.com>
2024-06-01 09:41:11 +0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -44,7 +44,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO POSTGRES DB
-app.config['SQLALCHEMY_DATABASE_URI'] = sqlite:///posts.db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy()
 db.init_app(app)
 
